9998b220b7d33ef46b422d1bc7fab83e8bc8957f
Varun <varunkumarpottigari@gmail.com>
2022-07-04 16:49:45 +0530

    changes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -226,6 +226,7 @@ async function loadInfo() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
+  console.log(info);
   const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'ether');
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
